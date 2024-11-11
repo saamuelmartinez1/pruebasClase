de50e222edf0ca8e9a9f86caecb4911798b40aec
sammarbla <samuelmartinezbla@gmail.com>
2024-11-11 10:18:11 +0100

    ejercicios bucle

--- src/tema2/ejercicio10.java
+++ src/tema2/ejercicio10.java
@@ -1,4 +1,6 @@
+package tema2;
 import java.util.Scanner;
+
 
 public class ejercicio10 {
     public static void main(String[] args) {
--- src/tema2/ejercicio3.java
+++ src/tema2/ejercicio3.java
@@ -1,3 +1,4 @@
+package tema2;
 import java.util.Scanner;
 
 public class ejercicio3 {
